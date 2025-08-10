1543baa20bb3e700e31c42a40a2b556f351e7a32
ashish-manna <m.ashish1920@gmail.com>
2025-08-10 18:57:33 +0530

    code refactored accrding to fronted dev requirements

--- src/route/profile.js
+++ src/route/profile.js
@@ -11,7 +11,7 @@ const profileRouter = express.Router();
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
-    res.status(200).json({ data: user });
+    res.status(200).json({ user: user });
   } catch (err) {
     res.status(404).json({ message: `Bad request` });
   }
