4fb85243482d5def19812c2db681b56c05bca5c8
ashish-manna <m.ashish1920@gmail.com>
2025-08-16 19:00:42 +0530

    cors error fixed

--- src/route/auth.js
+++ src/route/auth.js
@@ -41,7 +41,12 @@ authRouter.post("/login", async (req, res) => {
       throw new Error("Wrong Passwrod...");
     }
     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY);
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
     res.status(200).json({
       message: `Logged in sucessfully as ${user.firstName}`,
       user: user,
